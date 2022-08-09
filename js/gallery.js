dd6ff860795ce5564f06eeb3e0743f06c520e64d
TrungThang612 <2151050415thang@ou.edu.vn>
2022-08-09 20:31:19 +0700

    Đổi tên cái nữa t hốt m

---
+++ js/gallery.js
@@ -0,0 +1,46 @@
+// const glImage = document.getElementsByClassName('gl-image')
+const closeBtn = document.getElementById('gl-close')
+const gallery = document.getElementById('gallery')
+const prevBtn = document.getElementById('gl-prev')
+const nextBtn = document.getElementById('gl-next')
+const images = document.getElementsByClassName('gl-imagesrc')
+const galleryImg = document.getElementById('gl-imagezoom')
+var current = 0
+
+for (let i = 0; i < images.length; i++) {
+  images[i].addEventListener('click', function () {
+    current = i
+    galleryImg.src = this.src
+    gallery.classList.add('show')
+  })
+}
+
+closeBtn.addEventListener('click', function () {
+  gallery.classList.remove('show')
+})
+
+prevBtn.addEventListener('click', function () {
+if (current > 0){
+  current--;
+  if (current == 0){
+    prevBtn.classList.add('hide')
+  }else if (current!=0){
+    prevBtn.classList.remove('hide')
+  }// Thay src anh vao
+  galleryImg.src = images[current].src
+  gallery.classList.add('show')
+}
+})
+
+nextBtn.addEventListener('click', function () {
+  if (current < images.length - 1){
+    current++;
+    if (current == images.length - 1){
+      nextBtn.classList.add('hide')
+    }else if (current != images.length - 1){
+      nextBtn.classList.remove('hide')
+    }// Thay src anh vao
+    galleryImg.src = images[current].src
+    gallery.classList.add('show')
+  }
+})
